d5d48f73051497087598b3d210d052ab7c9238ff
Gonza Gomez <gonzalo.gomez.agg@gmail.com>
2024-10-13 22:55:53 -0300

    a

--- vite-project/src/app.jsx
+++ vite-project/src/app.jsx
@@ -1,48 +1,29 @@
-import {getItem, getPJ} from "/src/api.jsx" 
+import { getItem, getPJ } from "/src/api.jsx";
 import React, { useEffect, useState } from 'react';
+import "/src/app.css";
+import Items from '/src/componentes/item.jsx';
+
 
 export function App() {
-   
-    async function item(){
-        const resp = await getItem()
-        let sintaxis =  document.createElement("div")
-       
-        let currentDiv = document.getElementById("");
-        resp.map((item) =>{   
-        
+    const [items, setItems] = useState([]);
 
-          document.body.insertBefore(sintaxis, currentDiv);          
-          let newContent = document.createElement("p");
-          newContent.textContent = item
-          sintaxis.appendChild(newContent)
-
-          
-
-            console.log(item)
-            return{
-                id: item.id,
-                Name: item.Name,
-                Type: item.Type,
-                Value: item.Value,
-                durability: item.durability,
-                image: item.image
-        }}
-         )
-        
-        
+    async function fetchItems() {
+        const resp = await getItem();
+        setItems(resp);
     }
-    function PJ(){
-        const resp = getPJ()
-        console.log(resp)
-    }
-    return(
+
+    useEffect(() => {
+        fetchItems();
+    }, []);
+
+    return (
         <>
-        <button onClick={item}>obtener armas</button>
-        <button onClick={PJ}>obtener personaje </button>
-        <button>editar</button>
-        <button>crear</button>
+            <button onClick={fetchItems}>obtener armas</button>
+            <button onClick={getPJ}>obtener personaje</button>
+            <button>editar</button>
+            <button>crear</button>
 
-      </>
-    )
+            <Items items={items} />
+        </>
+    );
 }
-

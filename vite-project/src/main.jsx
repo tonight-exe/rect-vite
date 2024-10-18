830eba7fe1ed0570c6fc7d6f826b0a44eb00020a
Gonza Gomez <gonzalo.gomez.agg@gmail.com>
2024-10-18 20:28:08 -0300

    intento 30 para publicar git page

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import WrappedApp from './app.jsx';
 import "/src/index.css";
+import { App } from './app.jsx';
 
 // Obtén el elemento root
 const rootElement = document.getElementById('root');
@@ -13,8 +14,8 @@ const root = createRoot(rootElement);
 // Renderiza la aplicación
 root.render(
   <StrictMode>
-    <BrowserRouter>
-      <WrappedApp />
+    <BrowserRouter basename="/rect-vite/vite-project">
+    <App />
     </BrowserRouter>
   </StrictMode>
 );
